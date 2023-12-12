1a8e0c58c4f97306542519bc9c8efd9b52a52d64
tp.jp <chronokreuz@gmail.com>
2023-12-13 00:49:26 +0900

    全体的に処理を改善

--- Packages/com.tp.jp.vrc-url-input-field-resolver/Editor/Resolver.cs
+++ Packages/com.tp.jp.vrc-url-input-field-resolver/Editor/Resolver.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
-using System.Reflection;
+﻿using System;
+using System.Collections;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using VRC.SDKBase.Editor.Source.Helpers;
 
 namespace TpLab.VrcUrlInputFieldResolver.Editor
 {
@@ -12,67 +14,116 @@ namespace TpLab.VrcUrlInputFieldResolver.Editor
         [InitializeOnLoadMethod]
         static void OnLoad()
         {
-            EditorApplicationUtility.ProjectWasLoaded += ProjectWasLoaded;
+            EditorSceneManager.sceneOpened += OnSceneOpened;
         }
 
-        static void ProjectWasLoaded()
+        /// <summary>
+        /// シーンを開いた際に呼ばれるイベント。
+        /// </summary>
+        /// <param name="scene">開いたシーン</param>
+        /// <param name="mode">シーンを開くモード</param>
+        static void OnSceneOpened(Scene scene, OpenSceneMode mode)
         {
             // Missing check
-            if (!DetectMissingScripts())
+            if (!DetectMissingScripts(scene))
             {
                 return;
             }
+            Log("Resolving...");
 
-            // var message = "シーン内にMissingScriptが存在します。\nSDKのReloadとプレハブのReimportを実行しますか？";
-            // if (!EditorUtility.DisplayDialog("Confirm", message, "Yes", "No")) return;
+            // Reload SDK
+            Log("Reload SDK");
+            var targetPaths = FindFoldersWithMissingScripts();
+            ReloadUtil.ReloadSDK(false);
 
-            // SDK Reload
-            Debug.Log("Reload SDK");
-            EditorApplication.ExecuteMenuItem("VRChat SDK/Reload SDK");
-        
             // Reimport
-            Debug.Log("Reimport");
-            var paths = AssetDatabase
-                .FindAssets("t:prefab")
-                .Where(DetectMissingScriptsForPrefab);
-            foreach (var path in paths)
+            Log("Reimport");
+            foreach (var path in targetPaths)
             {
-                AssetDatabase.ImportAsset(path, ImportAssetOptions.Default);
+                AssetDatabase.ImportAsset(path, ImportAssetOptions.ImportRecursive);
             }
+
+            // Reopen scene
+            EditorCoroutine.Start(ReopenScene(scene));
         }
         
-        static bool DetectMissingScripts()
+        /// <summary>
+        /// プレハブ内の欠落しているスクリプトを検出し、対応するフォルダの一覧
+        /// </summary>
+        /// <returns></returns>
+        static string[] FindFoldersWithMissingScripts()
         {
-            var scene = EditorSceneManager.GetActiveScene();
+            return AssetDatabase
+                .FindAssets("t:prefab")
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Where(DetectMissingScriptsForPrefab)
+                .Select(x =>
+                {
+                    var pathArray = x.Split('/');
+                    return pathArray.Length > 1 ? $"{pathArray[0]}/{pathArray[1]}" : "";
+                })
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Distinct()
+                .ToArray();
+        }
+
+        /// <summary>
+        /// シーンを再度開く。
+        /// </summary>
+        /// <param name="scene">シーン</param>
+        /// <returns>コレクション参照IF</returns>
+        static IEnumerator ReopenScene(Scene scene)
+        {
+            yield return new WaitForSeconds(3f);
+            const int retryCount = 3;
+            for (var i = 0; i < retryCount; i++)
+            {
+                try
+                {
+                    EditorSceneManager.OpenScene(scene.path);
+                    Log("Resolved.");
+                    break;
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+                yield return new WaitForSeconds(1f);
+            }
+        }
+
+        /// <summary>
+        /// シーン内から欠落しているスクリプトを検出する。
+        /// </summary>
+        /// <param name="scene">対象シーン</param>
+        /// <returns>MissingScriptがある場合はtrue、それ以外はfalse</returns>
+        static bool DetectMissingScripts(Scene scene)
+        {
             return scene.GetRootGameObjects()
                 .SelectMany(x => x.GetComponentsInChildren<Component>(true))
                 .Any(x => x == null);
         }
 
+        /// <summary>
+        /// プレハブ内から欠落しているスクリプトを検出する。
+        /// </summary>
+        /// <param name="path">プレハブのパス</param>
+        /// <returns>MissingScriptがある場合はtrue、それ以外はfalse</returns>
         static bool DetectMissingScriptsForPrefab(string path)
         {
             var obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
             if (obj == null) return false;
-            return obj.GetComponentsInChildren<Component>(true)
+            return obj.GetComponentsInChildren<Transform>(true)
                 .Any(x => GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(x.gameObject) > 0);
         }
-    }
 
-    [InitializeOnLoad]
-    public static class EditorApplicationUtility
-    {
-        const BindingFlags BINDING_ATTR = BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic;
-        static readonly FieldInfo m_info = typeof(EditorApplication).GetField("projectWasLoaded", BINDING_ATTR);
-
-        public static UnityAction ProjectWasLoaded
+        /// <summary>
+        /// ログを出力する。
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        static void Log(string message)
         {
-            get { return m_info.GetValue(null) as UnityAction; }
-            set
-            {
-                var functions = m_info.GetValue(null) as UnityAction;
-                functions += value;
-                m_info.SetValue(null, functions);
-            }
+            Debug.Log($"[<color=#4EC9B0>VRCUrlInputFieldResolver</color>]: {message}");
         }
     }
 }
